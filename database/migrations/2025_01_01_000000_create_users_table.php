aa048bcc7ec700f3dd9049a805e4dab755a037d2
Pryape <bernardlereceveur@gmail.com>
2025-04-03 10:09:10 +0200

    migration

--- database/migrations/2025_01_01_000000_create_users_table.php
+++ database/migrations/2025_01_01_000000_create_users_table.php
@@ -14,23 +14,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        // Drop the users table if it exists
-        Schema::dropIfExists('users');
-
-        // Recreate the users table
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role')->default('utilisateur');
-            $table->string('profile_picture')->nullable();
-            $table->text('two_factor_secret')->nullable();
-            $table->text('two_factor_recovery_codes')->nullable();
-            if (Fortify::confirmsTwoFactorAuthentication()) {
-                $table->timestamp('two_factor_confirmed_at')->nullable();
-            }
             $table->rememberToken();
             $table->timestamps();
         });
@@ -43,7 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        // Drop the users table
         Schema::dropIfExists('users');
     }
 }
