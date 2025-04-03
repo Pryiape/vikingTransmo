86176886f8883eac4d137dc2cc4f475e377bf0d8
Pryape <bernardlereceveur@gmail.com>
2025-04-03 10:11:36 +0200

    migration

--- database/migrations/2025_04_03_080652_add_role_to_users_table.php
+++ database/migrations/2025_04_03_080652_add_role_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddRoleToUsersTable extends Migration
+class AddRoleToUsersTableUnique extends Migration
 {
     /**
      * Run the migrations.
