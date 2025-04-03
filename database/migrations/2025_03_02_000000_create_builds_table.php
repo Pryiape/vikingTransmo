e31ad4641210608897f1d2bd0dfe0cf64c6446ec
Pryape <bernardlereceveur@gmail.com>
2025-04-03 12:15:08 +0200

    migration

--- database/migrations/2025_03_02_000000_create_builds_table.php
+++ database/migrations/2025_03_02_000000_create_builds_table.php
@@ -4,29 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddRoleToUsersTable extends Migration
+class CreateBuildsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default('utilisateur');
+        Schema::create('builds', function (Blueprint $table) {
+            $table->id();
+            $table->string('sujet');
+            $table->text('description')->nullable();
+            $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('role');
-        });
+        Schema::dropIfExists('builds');
     }
 }
